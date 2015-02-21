5e405a75e40dffbbdde1f6ed9e2baeb6863fcb5c
Joe McGill <joemcgill@gmail.com>
2015-02-21 14:12:07 -0600

    Button and CTA styles (with tests)

---
+++ layouts/patterns/test-buttons.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Button test
+ *
+ * @package rfm
+ */
+?>
+<h2>Button test</h2>
+
+<div>
+  <div class="btn btn-sm">Button</div>
+  <div class="btn btn-sm btn-primary">Button</div>
+  <div class="btn btn-sm btn-secondary">Button</div>
+</div>
+
+<div>
+  <a href="#" class="btn btn-lg">Button</a>
+  <a href="#" class="btn btn-lg btn-primary">Button</a>
+  <a href="#" class="btn btn-lg btn-secondary">Button</a>
+</div>
+
+<div>
+  <button class="btn">Button</button>
+  <button class="btn btn-primary">Button</button>
+  <button class="btn btn-secondary">Button</button>
+</div>
+
+<div>
+  <button class="btn btn-block">Button</button>
+  <a href="#" class="btn btn-block btn-primary">Button</a>
+  <span class="btn btn-block btn-secondary">Button</span>
+</div>
