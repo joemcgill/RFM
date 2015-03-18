91741ce32f2294b6c5211f6d48f1e28e50c6f2fd
Joe McGill <joemcgill@gmail.com>
2015-03-17 22:28:16 -0500

    remove comments from page template

--- page.php
+++ page.php
@@ -19,19 +19,12 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
 
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				?>
-
 			<?php endwhile; // end of the loop. ?>
 
 		</main><!-- #main -->
 
 		<?php get_sidebar(); ?>
-		
+
 	</div><!-- #primary -->
 
 <?php get_footer(); ?>
