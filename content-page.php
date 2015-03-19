ede56a6eca2bcb5958a9eaf01e5e00e98f8933ff
Joe McGill <joemcgill@gmail.com>
2015-03-19 15:22:25 -0500

    Add section header to page templates + styles

--- content-page.php
+++ content-page.php
@@ -8,6 +8,12 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<?php
+			if ( is_page() && $post->post_parent > 0 ) {
+		?>
+		<a class="section-label" href="<?php echo get_permalink($post->post_parent); ?>"><?php echo get_the_title($post->post_parent); ?></a>
+		<?php } // end if ?>
+
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
