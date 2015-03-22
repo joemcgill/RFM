c6b87e34d0d464e46ea86bb5dc8ce76604581682
Joe McGill <joemcgill@gmail.com>
2015-03-22 17:33:16 -0500

    php style errors

--- content.php
+++ content.php
@@ -5,9 +5,9 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail( $post->ID, 'large') ) { ?>
+	<?php if ( has_post_thumbnail( $post->ID, 'large' ) ) { ?>
 	<figure class="featured-image">
-		<?php the_post_thumbnail('large'); ?>
+		<?php the_post_thumbnail( 'large' ); ?>
 	</figure>
 	<?php } ?>
 
