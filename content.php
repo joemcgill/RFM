9a1342c6e714581312d181465742be4a0a864e82
Joe McGill <joemcgill@gmail.com>
2015-03-22 14:54:42 -0500

    Add featured images to posts.

--- content.php
+++ content.php
@@ -5,6 +5,12 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php if ( has_post_thumbnail( $post->ID, 'large') ) { ?>
+	<figure class="featured-image">
+		<?php the_post_thumbnail('large'); ?>
+	</figure>
+	<?php } ?>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
